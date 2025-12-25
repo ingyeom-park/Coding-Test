ccdc5c8e4b94328f7f367a995ff950fa6dda53b3
PARK INGYEOM <68932926+ingyeom-park@users.noreply.github.com>
2025-12-26 01:18:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.73 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,27 +1,13 @@
 def solution(participant, completion):
-    people_dict = {}
-    # 참가자들의 정보를 처리할 변수
-    # k = 참가자 이름
-    # v = 이름에 대한 참가자 수
-
-    # 입력으로 주는 참가자 명단을 보면서 정리: 순회 > for문
-    for p in participant:
-        if p in people_dict: #기등록 이름
-            people_dict[p] += 1
-        else:                #t신규등록 이름
-            people_dict[p] = 1
-
-    #완주자 명단을 순회하기
-    for c in completion:
-        # 완주자 이름 1명씩 불러다가 할 것.
-        # 기준: 이름에 대한 사람 수(v쪽 값이 1이냐, 2냐 등)
-
-        # 1명이 등록되어있고, 그 1명이 완주했다면: 완전 제명
-        if people_dict[c] == 1: del people_dict[c]
-        # 아직 동명이인 2명 이상 남아있다면?
-        else: people_dict[c] -= 1
-
-        # 이렇게 계속 순회 순회 순회 한다면, people_dict의 키 값에 남아있게 된다. 
+    # 1. 두 리스트를 모두 정렬합니다.
+    participant.sort()
+    completion.sort()
     
-    answer = list(people_dict.keys())[0]
-    return answer
+    # 2. 앞에서부터 하나씩 비교합니다.
+    for i in range(len(completion)):
+        # 만약 순서가 맞지 않는 이름이 나온다면, 그 사람이 완주하지 못한 사람입니다.
+        if participant[i] != completion[i]:
+            return participant[i]
+            
+    # 3. 끝까지 다 똑같다면, 마지막 남은 한 사람이 범인입니다.
+    return participant[-1]
