202d0d859d28c121cb0d618f1ec1047b2613ca4a
PARK INGYEOM <68932926+ingyeom-park@users.noreply.github.com>
2025-12-26 16:17:43 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    vowel = ["a", "e", "i", "o", "u"]
+    answer = ''
+
+    for i in my_string:
+        if i not in vowel:
+            answer += i
+        else:
+            pass
+    return (answer)
