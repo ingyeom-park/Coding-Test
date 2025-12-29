f9a264f1b41550ec5c2264a2f8bbae76343e70e8
PARK INGYEOM <68932926+ingyeom-park@users.noreply.github.com>
2025-12-29 09:33:55 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,6 +1,2 @@
 def solution(my_string):
-    answer = []
-    for i in my_string:
-        if i.isdigit():
-            answer.append(int(i))
-    return sorted(answer)
+    return sorted([int(x) for x in my_string if x.isdigit()])
