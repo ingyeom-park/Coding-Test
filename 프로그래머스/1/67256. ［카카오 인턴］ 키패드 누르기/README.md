2e2ae24f1fac49b741210973a6fa9cb8ef088494
PARK INGYEOM <68932926+ingyeom-park@users.noreply.github.com>
2025-12-31 17:30:13 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.71 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.32 MB, 시간: 0.91 ms
+메모리: 9.3 MB, 시간: 0.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 23일 09:25:19
+2025년 12월 31일 17:30:11
 
 ### 문제 설명
 
--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,47 +1,68 @@
 def solution(numbers, hand):
-    numbers_pos = {
-    1 : (1, 1),
-    2 : (1, 2),
-    3 : (1, 3), 
-    4 : (2, 1),
-    5 : (2, 2),
-    6 : (2, 3),
-    7 : (3, 1), 
-    8 : (3, 2),
-    9 : (3, 3),
-    0 : (4, 2)
-}
-    # 왼손으로 누르면 L, 오른손으로 누르면 R을 차례대로 저장
+    # 번호마다의 좌표를 딕셔너리 형태로 저장
+    num_pos = {
+        1 :   (0, 0), 
+        2 :   (0, 1),
+        3 :   (0, 2),
+        4 :   (1, 0), 
+        5 :   (1, 1),
+        6 :   (1, 2),
+        7 :   (2, 0),
+        8 :   (2, 1),
+        9 :   (2, 2),
+        "*": (3, 0),
+        0 :   (3, 1),
+        "#": (3, 2)
+    }
+
+    # 왼손과 오른손의 시작 좌표 설정
+    left_pos = num_pos["*"]
+    right_pos = num_pos["#"]
+
+    # 결과를 담을 result 리스트
     result = []
 
-    left_pos = (4, 1)
-    right_pos = (4, 3)
-
-    for i in numbers:
-        if i == 1 or i == 4 or i == 7:
-            left_pos = numbers_pos[i]
+    # numbers 배열의 값을 하나씩 순회
+    for x in numbers:
+        
+        # x가 왼손으로 눌러야 하는 키일 경우
+        if x in [1, 4, 7]:
+            left_pos = num_pos[x]
             result.append("L")
-
-        elif i == 3 or i == 6 or i == 9:
-            right_pos = numbers_pos[i]
+        
+        # x가 오른손으로 눌러야 하는 키일 경우
+        elif x in [3, 6, 9]:
+            right_pos = num_pos[x]
             result.append("R")
-
-        elif i == 2 or i == 5 or i == 8 or i == 0:
-            target_pos = numbers_pos[i]
-            if (abs(target_pos[0] - left_pos[0]) + abs(target_pos[1] - left_pos[1])) < (abs(target_pos[0] - right_pos[0]) + abs(target_pos[1] - right_pos[1])):
-                left_pos = numbers_pos[i]
+        
+        # x가 중간에 있어 판가름을 해야 하는 경우
+        else: # x == [2, 5, 8, 0]
+            
+            # 왼손과 오른손의 키(x) 간의 절대 거리를 계산
+            left_diff = abs(num_pos[x][0]-left_pos[0]) + abs(num_pos[x][1]-left_pos[1])
+            right_diff = abs(num_pos[x][0]-right_pos[0]) + abs(num_pos[x][1]-right_pos[1])
+            
+            # 왼손의 차이가 더 작을 경우 (왼손이 더 가까움)
+            if left_diff < right_diff:
+                left_pos = num_pos[x]
                 result.append("L")
-            elif (abs(target_pos[0] - left_pos[0]) + abs(target_pos[1] - left_pos[1])) > (abs(target_pos[0] - right_pos[0]) + abs(target_pos[1] - right_pos[1])):
-                right_pos = numbers_pos[i]
+            
+            # 오른손의 차이가 더 작을 경우 (오른손이 더 가까움)  
+            elif left_diff > right_diff:
+                right_pos = num_pos[x]
                 result.append("R")
-            elif (abs(target_pos[0] - left_pos[0]) + abs(target_pos[1] - left_pos[1])) == (abs(target_pos[0] - right_pos[0]) + abs(target_pos[1] - right_pos[1])):
+            
+            # 둘의 거리가 같을 경우
+            elif left_diff == right_diff:
+                
+                # 왼손잡이인지 오른손잡이인지로 판가름
                 if hand == "left":
-                    left_pos = numbers_pos[i]
+                    left_pos = num_pos[x]
                     result.append("L")
-                else:
-                    right_pos = numbers_pos[i]
+                    
+                elif hand == "right":
+                    right_pos = num_pos[x]
                     result.append("R")
-
-    result = ''.join(result)
-
-    return result
+                    
+    # 결과를 join 함수를 통해 보기좋게 치환, return       
+    return "".join(result)
